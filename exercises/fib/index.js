850a943c1055f3d68d628a470386aa88c81b68b7
Joseph Fajardo <jafajardo@gmail.com>
2021-07-13 10:11:06 +1000

    finished exercises

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,17 +8,65 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {
-  let x = 0;
-  let counter = 1;
-  let prev = 0;
-  for (let i = 1; i <= n; i++) {
-    x = counter;
-    counter = counter + prev;
-    prev = x;
-  }
+function memoize(fn) {
+  const cache = {};
+  return function (...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
 
-  return prev;
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
 }
 
+function fib(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+}
+
+fib = memoize(fib);
+
 module.exports = fib;
+
+// function fib(n, pos = 0, res = [0, 1]) {
+//   if (pos === n) {
+//     return;
+//   }
+
+//   res.push(res[pos] + res[pos + 1]);
+//   pos++;
+//   fib(n, pos, res);
+//   return res[n];
+// }
+
+// function fib(n) {
+//   const result = [0, 1];
+
+//   for (let i = 2; i <= n; i++) {
+//     const a = result[i - 1];
+//     const b = result[i - 2];
+
+//     result.push(a + b);
+//   }
+
+//   return result[n];
+// }
+
+// function fib(n) {
+//   let x = 0;
+//   let counter = 1;
+//   let prev = 0;
+//   for (let i = 1; i <= n; i++) {
+//     x = counter;
+//     counter = counter + prev;
+//     prev = x;
+//   }
+
+//   return prev;
+// }
